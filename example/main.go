91b94e7a5ee1daec848dded14b48e1871dc9ba16
priyon999 <priyon999@gmail.com>
2025-05-03 22:50:53 +0530

    refactor code

--- example/main.go
+++ example/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"github.com/Priyokumar/goemail"
@@ -10,31 +11,28 @@ func main() {
 
 	m, err := goemail.New(
 		goemail.ConnectionDetails{
-			Host:         "w",
-			Port:         3,
+			SmtpHost:     "w",
+			SmtpPort:     3,
 			SmtpUser:     "w",
 			SmtpPassword: "e",
-		},
-		goemail.MailDetails{
-			To:          []string{"priyon999@gmail.com"},
-			Subject:     "Test",
-			Sender:      "priyon999@gmail.com",
-			SenderName:  "Test",
-			ReturnEmail: "priyon999@gmail.com",
-			ContentType: goemail.ContentHTML,
-			Template: goemail.Template{
-				Multilevel:    false,
-				TemplatePaths: []string{"index.html"},
-				Data:          nil,
-			},
 		})
 
 	if err != nil {
 		log.Println(err)
 		return
 	}
-
-	err = m.Send()
+	m.SetTo([]string{"priyon999@gmail.com"})
+	m.SetSubject("Test")
+	m.SetSenderName("test")
+	m.SetSender("test@test.com")
+	m.SetReturnEmail("tets@test.com")
+	m.SetContent(
+		goemail.Content{
+			Type:    goemail.ContentText,
+			Content: "This is test email.",
+		},
+	)
+	err = m.Send(context.Background(), 1)
 
 	if err != nil {
 		log.Println(err)
